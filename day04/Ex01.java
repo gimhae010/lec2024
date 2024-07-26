e1788a26a7e16ef6d3f433d8c9f49a6d530f9ae0
gimhae010 <gimhae010@outlook.com>
2024-07-26 17:32:23 +0900

    1주차

--- day04/Ex01.java
+++ day04/Ex01.java
@@ -1,6 +1,9 @@
 package one;
 class Ex01{
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println(args.length);
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
     }
 }
