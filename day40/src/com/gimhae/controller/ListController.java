5fcb941a643794bd2b2c0301df906603c9822bcc
gimhae010 <gimhae010@outlook.com>
2024-09-23 14:54:35 +0900

    jdbcTemplate step1

---
+++ day40/src/com/gimhae/controller/ListController.java
@@ -0,0 +1,24 @@
+package com.gimhae.controller;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gimhae.framework.mvc.MyController;
+import com.gimhae.model.EmpDao;
+
+public class ListController implements MyController {
+
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse arg1) {
+		EmpDao dao=new EmpDao();
+		try {
+			req.setAttribute("alist", dao.pullList());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "emp/list";
+	}
+
+}
