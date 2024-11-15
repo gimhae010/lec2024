b98e58fa683dc4ebe9d7cef580dd120ac8a15874
gimhae010 <gimhae010@outlook.com>
2024-11-15 15:53:13 +0900

    express & mysql

--- node03/app.js
+++ node03/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const parser=require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine', 'ejs')
+app.use(parser.json()) // for parsing application/json
+app.use(parser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use('/',require('./routes/homeController'));
 app.use('/dept/',require('./routes/deptController'));
--- node03/routes/deptController.js
+++ node03/routes/deptController.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
     // const arr=[];
     // for(var i=1; i<6; i++)
     //     arr.push({deptno:i,dname:'user'+i,loc:'test'});
-    const sql="select * from dept order by deptno desc";
+    const sql="select * from dept order by deptno asc";
     const conn=mysql.createConnection(info);
     const callback=err=>{
         if(err) throw err;
@@ -31,8 +31,49 @@ router.get('/add',(req,res)=>{
 });
 
 router.post('/',(req,res)=>{
-
-    res.redirect('./');
+    const {deptno,dname,loc}=req.body;
+    const sql=`insert into dept values (${deptno},'${dname}','${loc}')`;
+    const conn=mysql.createConnection(info);
+    conn.connect(err=>{
+        if(err) throw err;
+        conn.query(sql,err2=>{
+            console.log(err2);
+            conn.end();
+            res.redirect('./');
+        });
+    });
 });
+
+router.get("/detail",(req,res)=>{
+    const deptno=Number(req.query.deptno);
+    const sql=`select * from dept where deptno=?`;
+    const conn=mysql.createConnection(info);
+    conn.connect(err=>conn.query(sql,[deptno],(err2,result)=>{
+        const {DEPTNO,DNAME,LOC}=result[0];
+        res.render('dept/detail',{readonly:'readonly',title:'detail',deptno:DEPTNO,dname:DNAME,loc:LOC});
+    }));
+});
+
+router.get("/edit",(req,res)=>{
+    const deptno=Number(req.query.deptno);
+    const sql=`select * from dept where deptno=?`;
+    const conn=mysql.createConnection(info);
+    conn.connect(err=>conn.query(sql,[deptno],(err2,result)=>{
+        const {DEPTNO,DNAME,LOC}=result[0];
+        res.render('dept/detail',{readonly:null,title:'update',deptno:DEPTNO,dname:DNAME,loc:LOC});
+    }));
+});
+
+
+router.put("/edit",(req,res)=>{
+    console.log(req.body);
+    const {deptno,dname,loc}=req.body;
+    const sql=`update dept set dname=?,loc=? where deptno=?`;
+    const conn=mysql.createConnection(info);
+    conn.connect(err=>conn.query(sql,[dname,loc,deptno],(err,result)=>{
+        res.json({err,result:result.affectedRows});
+    }));
+});
+
 
 module.exports=router;
