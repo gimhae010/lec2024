88e1afd10e9735c341a00ce9bf9234564e3ddba5
gimhae010 <gimhae010@outlook.com>
2024-11-18 10:03:28 +0900

    express-generator init

---
+++ node05/modules/mysql.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: 'localhost',
+  database: 'xe',
+  user: 'scott',
+  password: 'tiger',
+  connectionLimit: 5
+});
