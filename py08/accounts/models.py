e9e4ca45a72af26ef236c29fdab6b6f1397255e8
gimhae010 <gimhae010@outlook.com>
2024-11-29 16:44:26 +0900

    django CRUD & LOGIN

--- py08/accounts/models.py
+++ py08/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username=models.CharField(max_length=5)
-    email=models.CharField(max_length=20,db_index=True)
-    password=models.CharField(max_length=16)
+# class User(models.Model):
+#     username=models.CharField(max_length=5)
+#     email=models.CharField(max_length=20,db_index=True)
+#     password=models.CharField(max_length=16)
--- py08/accounts/views.py
+++ py08/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.models import User
-# from django.contrib.auth import login, logout, authenticate
-# from django.contrib.auth.decorators import login_required
-from accounts.models import User
+#############장고 auth#####################
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+#############사용자 정의 로그인 테이블###########################
+# from accounts.models import User
 from django.http import HttpResponse
 
 
@@ -17,13 +19,15 @@ def user_signup(request):
             return HttpResponse("유저이름이 이미 있습니다.")
         if email and User.objects.filter(email=email).exists():
             return HttpResponse("이메일이 이미 있습니다.")
-        # user = User.objects.create_user(username, email, password)
-        # user.save()
-        # user = authenticate(username=username, password=password)
-        user=User(username=username,email=email,password=password)
+
+        user = User.objects.create_user(username, email, password)
         user.save()
+        # user = authenticate(username=username, password=password)
         # login(request, user)
         # return redirect("user_profile")
+        #############사용자 정의 로그인 테이블###########################
+        # user=User(username=username,email=email,password=password)
+        # user.save()
         return redirect("user_login")
     else:
         return render(request, "accounts/signup.html")
@@ -33,12 +37,13 @@ def user_login(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        # user = authenticate(request, username=username, password=password)
-        user=User.objects.filter(username=username,password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            # login(request, user)
-            request.session['username']=username
-            # request.session['email']=user.email
+            login(request, user)
+        #############사용자 정의 로그인 테이블###########################
+        # user = User.objects.filter(username=username, password=password)
+        # if user is not None:
+        #     request.session['username'] = username
             return redirect("user_profile")
         else:
             return render(
@@ -51,20 +56,21 @@ def user_login(request):
 
 
 def user_logout(request):
-    # logout(request)
-    # print(dir(request.session))
-    print(request.session.session_key,request.session.get('username'))
-    # request.session.pop()
-    request.session.clear()
-    print(request.session.session_key,request.session.get('username'))
+    logout(request)
+    #########################################
+    # print(request.session.session_key,request.session.get('username'))
+    # request.session.clear()
+    # print(request.session.session_key,request.session.get('username'))
     return redirect("user_login")
 
 
-# @login_required
+@login_required(login_url="user_login")
 def user_profile(request):
-    print(request.session.session_key)
-    sessionUser=request.session.get('username')
-    if sessionUser!=None:
-        return render(request, "accounts/profile.html", {"user": sessionUser})
-    else :
-        return redirect("user_login")
+    return render(request, "accounts/profile.html", {"user": request.user})
+    # print(request.session.session_key)
+    # sessionUser=request.session.get('username')
+    # sessionUser=User.objects.get(username=username)
+    # if sessionUser!=None:
+    #     return render(request, "accounts/profile.html", {"user": sessionUser})
+    # else :
+    #     return redirect("user_login")
